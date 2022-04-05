32908e5402341936236b3b22b9984ae75d075407
Wan Chin <gohwanchin@hotmail.com>
2022-04-05 11:07:20 +0800

    initial commit

---
+++ src/test/java/sg/edu/nus/day22/Day22ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.day22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
